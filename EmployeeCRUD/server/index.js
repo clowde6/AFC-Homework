c2a32ca0267c1a3a808695194778a0cec4462936
Corbett <corbett.m.lowder.mil@army.mil>
2023-05-30 13:09:35 -0500

    Delete Route

--- EmployeeCRUD/server/index.js
+++ EmployeeCRUD/server/index.js
@@ -7,7 +7,11 @@ const logger = require("morgan")
 const port = process.env.PORT || 3000
 app.use(logger("dev"))
 
-//CONECTIONS
+//body parser
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+//CONNECTIONS
 //Need the env variables we created
 const { PG_HOST, PG_PORT, PG_DB, PG_USER, PG_PW } = process.env;
 const credentials = {
@@ -38,9 +42,7 @@ app.get("/", (req, res) => {
 
 //NOTE: Your boss says:
 //need five route handlers -CRUD
-//1) star handler
-    // deals with any unmatched routes
-    // right now - probably send a string back with comments
+
 
 
 
@@ -53,27 +55,58 @@ app.get("/employee/all", (req, res) => {
     let query = `SELECT *
                  FROM employees;`
     conn.query(query)
-    .then(data => {
+    .then((data) => {
         res.json(data.rows)
     }).catch(err => res.send(`Error reading data: ${err}`))
 })
 
 //3) /emplyee/new --CREATE
-    // POST method
-    // send back JSON
+    // POST method - DONE
+    // send back JSON - DONE
         // one thing - data that DB confirmed was added
         // one object - BUT inital return maybe an array of objects
+    // NOTE: we need to take ONE bject from client and add to our DB
+    app.post("/employee/new", (req, res) => {
+        // data will be sent from client as body
+        const {first_name, last_name, age, salary} = req.body
+        let query = `INSERT INTO employees (first_name, last_name, age, salary)
+        VALUES('${first_name}','${last_name}',${age},${salary})
+        RETURNING *`;
+    conn.query(query)
+    .then((data) => {
+        console.log("DATA has been added to DB")
+        res.json(data.rows[0]);
+    }).catch(err => res.send(`Error creating data: ${err}`))
+    })
+
+
 //4) /employee/<<id>> - UPDATE
     // PUT method
     // send JSON
     // one thing - data that DB confirmed was updated
     // one object - BUT inittial return maybe an array of object
+
+
 //5) /employee/<<id>> - DELETE
-    // DELETE method
+    // DELETE method - DONE
     // send JSON
     // one thing - data that DB confirmed was updated
     // one object - BUT inittial return maybe an array of object
+    app.delete("/employee/:is", (req, res) => {
+        let query = `DELETE FROM employees
+                     WHERE employee_is = ${Number(req.params.is)}
+                     RETURNING *;`
+    conn.query(query)
+    .then((data) => {
+        console.log("DATA has been deleted to DB")
+        res.json(data.rows[0]);
+    }).catch(err => res.send(`Error deleted data from server: ${err}`))
+    })
 
+
+//1) star handler
+    // deals with any unmatched routes
+    // right now - probably send a string back with comments
 app.get("*", (req, res) => {
     res.send("No routes matching")
 })
