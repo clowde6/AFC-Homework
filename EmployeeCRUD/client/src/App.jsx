245356487ecffed8a121d3c9477d265399a9ddcc
Corbett <corbett.m.lowder.mil@army.mil>
2023-06-01 09:33:44 -0500

    Themes Done

--- EmployeeCRUD/client/src/App.jsx
+++ EmployeeCRUD/client/src/App.jsx
@@ -4,7 +4,7 @@ import Table from "./components/Table"
 import Landing from "./components/Landing"
 import Error from "./components/Error"
 import NavBar from "./components/NavBar"
-import { ThemeProvider } from "@mui/material"
+import { ThemeProvider, Typography } from "@mui/material"
 import Theme from "./components/ui/Theme"
 
 //importing styling stuff
@@ -15,8 +15,15 @@ function App() {
   return (
   <>
   <ThemeProvider theme={Theme}>
+    <Typography 
+      variant="h4"
+      sx={{color:"primary.main"
+          // "&:hover" : {backgroundColor:"yellow"}
+    }}
+    >
+    <h1>Employees R' us</h1>
+    </Typography>
   <NavBar/>
-  <h1>Employees R' us</h1>
 
   <Router>
     <Link to="/employee/table">All Employees</Link>
