90e57a52f9afc67c045f63b690bf5ed21b32290e
Corbett <corbett.m.lowder.mil@army.mil>
2023-06-06 15:06:21 -0500

    Error Page working

--- TMDBReact/src/components/Card.jsx
+++ TMDBReact/src/components/Card.jsx
@@ -4,24 +4,20 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { CardActions, useTheme } from "@mui/material";
+import error_img from "../assets/images/error_tag.jpeg";
 
 const MediaCard = (props) => {
   console.log(props);
-  // const theme = useTheme();
-  // let styles = {
-  //   height: 250,
-  //   border: "solid green 5px",
-  // };
 
   const { title, image_path, overview, poster_path } = props.data;
   let baseUrl = "https://image.tmdb.org/t/p/w500";
   console.log(image_path);
 
   return (
-    <Card sx={{ maxWidth: 300, display: "inline-block" }}>
+    <Card sx={{ maxWidth: 400, display: "inline-block" }}>
       <CardMedia
         sx={{ height: 400 }}
-        image={`${baseUrl}/${poster_path}`}
+        image={poster_path === null ? error_img : `${baseUrl}/${poster_path}`}
         title={title}
       />
       <CardContent sx={{ height: 200, overflowY: "auto" }}>
--- TMDBReact/src/components/Error.jsx
+++ TMDBReact/src/components/Error.jsx
@@ -1,11 +1,19 @@
-
 const Error = () => {
   return (
     <div className="error-page">
       <h1 className="error-title">Oops, something went wrong!</h1>
-      <p className="error-message">It seems that our monkey developers are playing around with the code.</p>
-      <img className="error-image" src="https://www.example.com/error-monkey.gif" alt="Error Monkey" />
-      <p className="error-hint">To fix the issue, please try refreshing the page or contact our support team.</p>
+      <p className="error-message">
+        It seems that our monkey developers are playing around with the code.
+      </p>
+      <img
+        className="error-image"
+        src="./src/assets/images/laugh.gif"
+        alt="Funny Error Message"
+      />
+      <p className="error-hint">
+        To fix the issue, please try refreshing the page or contact our support
+        team.
+      </p>
     </div>
   );
 };
