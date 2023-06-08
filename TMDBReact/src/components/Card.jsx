175ea393ee5ac35b79fa1f326d5ab782c9abc5f2
Corbett <corbett.m.lowder.mil@army.mil>
2023-06-08 11:26:33 -0500

    Comment Card

--- TMDBReact/src/components/Card.jsx
+++ TMDBReact/src/components/Card.jsx
@@ -1,17 +1,20 @@
-import Card from "@mui/material/Card";
+// Functional Import
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import Card from "@mui/material/Card";
+
+//Styling Import
 import Typography from "@mui/material/Typography";
 import error_img from "../assets/images/error_tag.jpeg";
 
 const MediaCard = (props) => {
-  const { title, overview, poster_path } = props.data;
-  let baseUrl = "https://image.tmdb.org/t/p/w500";
+  const { title, overview, poster_path } = props.data; // Destructure the data props to extract title, overview, and poster_path
+  let baseUrl = "https://image.tmdb.org/t/p/w500"; // Define the base URL for fetching movie poster images
 
   return (
     <Card
       sx={{
-        width: "calc(30% - 16px)", // Set the width to 50% minus the horizontal margins (16px)
+        width: "calc(30% - 16px)", // Set the width to 30% minus the horizontal margins (16px)
         display: "inline-block",
         borderRadius: "8px",
         objectFit: "scale-down",
@@ -26,8 +29,9 @@ const MediaCard = (props) => {
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
         }}
+        // Set the image source to either the poster_path or the error image //
         image={poster_path === null ? error_img : `${baseUrl}/${poster_path}`}
-        title={title}
+        title={title} // Set the image title to the movie title
       />
       <CardContent sx={{ height: 250, overflowY: "scroll" }}>
         <Typography gutterBottom variant="h5" component="div">
