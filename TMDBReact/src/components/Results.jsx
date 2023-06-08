0d0821164600f035271c759a23319ee2f07a66ec
Corbett <corbett.m.lowder.mil@army.mil>
2023-06-08 11:02:46 -0500

    Commented Results

--- TMDBReact/src/components/Results.jsx
+++ TMDBReact/src/components/Results.jsx
@@ -1,11 +1,15 @@
+// ////////////////////////////////////////////////////////////////////// //
+// This component is responsible for rendering the results of the movies. //
+// ////////////////////////////////////////////////////////////////////// //
+
+// Importing the MediaCard component to display individual movie cards.
 import MediaCard from "./Card";
 
 const Results = (props) => {
-  //   console.log(props.data[0]);
   let displayMovies = props.data.map((el, i) => {
-    return <MediaCard data={el} key={i} />;
+    return <MediaCard data={el} key={i} />; // Creating a MediaCard component for each movie in the data array
   });
-  return <>{displayMovies}</>;
+  return <>{displayMovies}</>; // Render the array of MediaCard components.
 };
 
 export default Results;
