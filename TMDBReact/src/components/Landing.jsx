43dc99df816e94d2b1de5a64a4a7840fb23cbac3
Corbett <corbett.m.lowder.mil@army.mil>
2023-06-08 11:14:37 -0500

    Comment Landing

--- TMDBReact/src/components/Landing.jsx
+++ TMDBReact/src/components/Landing.jsx
@@ -1,3 +1,8 @@
+// ////////////////////////////////////////////////////////////// //
+// This component represents the landing page of the application. //
+// ////////////////////////////////////////////////////////////// //
+
+// Functional Imports
 import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 
@@ -5,7 +10,9 @@ const Landing = (props) => {
   return (
     <>
       <h1> Want to see a movie? </h1>
+      {/* Create a Link component to navigate to the "/movies/new" route. */}
       <Link to="/movies/new">
+        {/* Render a Button component with the label "Now Playing". */}
         <Button variant="contained">Now Playing</Button>
       </Link>
     </>
